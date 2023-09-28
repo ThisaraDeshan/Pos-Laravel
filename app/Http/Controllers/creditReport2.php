08b892288a6835dcaab383d0dd5eb8d8ff28fb15
ThisaraDeshan <125489678+ThisaraDeshan@users.noreply.github.com>
2023-09-28 21:33:04 +0530

    2023/09/28

---
+++ app/Http/Controllers/creditReport2.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class creditReport2 extends Controller
+{
+    //
+}
---
+++ public/tt/1.php
@@ -0,0 +1,6 @@
+<?php
+$now = new DateTime("2010-07-28 01:11:50");
+$ref = new DateTime("2010-07-30 05:56:40");
+$diff = $now->diff($ref);
+
+?>
